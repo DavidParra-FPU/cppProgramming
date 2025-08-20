85d4f4259544c6782d9259ede2f33d5f220354a1
DavidParra-FPU <dparrajimenez8640@floridapoly.edu>
2025-08-20 18:42:17 +0000

    Created unix instructions

---
+++ first_program.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+//using namespace std; <------ Optional
+//using std::cout; <---------- Optional
+
+int main(void){
+    std::cout << "Hello!" << std::endl;
+    //std = standard namespace
+    //:: - scope resolution operator
+    //cout - console output (object of an iostream class)
+    //<< - stream insertion operator
+    //endl - end of line
+
+    return 0;
+}
